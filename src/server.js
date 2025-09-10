f775d637445daa8210049e6133d2a52505bccffd
betymesfin <98676637+betymesfin@users.noreply.github.com>
2025-09-09 23:33:14 -0500

    installed mocha and chai

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ const connectDB = require("./db/connect");
 const { PORT = 8000 } = process.env;
 const app = require("./app");
 
+let mongoURL = process.env.MONGO_URI;
+if (process.env.NODE_ENV == "test") {
+  mongoURL = process.env.MONGO_URI_TEST;
+}
+
 const listener = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(mongoURL);
 
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}...`);
